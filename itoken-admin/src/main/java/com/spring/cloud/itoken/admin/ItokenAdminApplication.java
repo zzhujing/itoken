fa4b88f0722ab9942d68f4e3cd7c0fdd40a80e2e
hujing <hj_960825@163.com>
2019-03-11 23:52:20 +0800

    提交admin配置到配置中心

--- itoken-admin/src/main/java/com/spring/cloud/itoken/admin/ItokenAdminApplication.java
+++ itoken-admin/src/main/java/com/spring/cloud/itoken/admin/ItokenAdminApplication.java
@@ -1,9 +1,13 @@
 package com.spring.cloud.itoken.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class ItokenAdminApplication {
 
     public static void main(String[] args) {
