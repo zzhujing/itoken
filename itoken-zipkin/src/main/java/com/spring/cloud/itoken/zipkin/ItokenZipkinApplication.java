1c009372e915113ed1644bb84237cb82ab14ddc3
hujing <hj_960825@163.com>
2019-03-11 23:32:06 +0800

    提交zipkin配置到配置中心

--- itoken-zipkin/src/main/java/com/spring/cloud/itoken/zipkin/ItokenZipkinApplication.java
+++ itoken-zipkin/src/main/java/com/spring/cloud/itoken/zipkin/ItokenZipkinApplication.java
@@ -2,8 +2,12 @@ package com.spring.cloud.itoken.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
 public class ItokenZipkinApplication {
 
     public static void main(String[] args) {
